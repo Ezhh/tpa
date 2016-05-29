d47ba0c8095ad540e39d3402ad4d56d13d76ca60
ezhh <owlecho@live.com>
2016-05-30 00:17:11 +0100

    Removed old code

--- init.lua
+++ init.lua
@@ -1,31 +1,4 @@
 
-
--- --area teleportation
--- minetest.register_chatcommand("tpa", {
--- 	params = "<id>",
--- 	privs = {noclip = true, areas = true},
--- 	description = "Teleport to an area using its <id>.",
--- 	func = function(name, param)
--- 		local target_id = tonumber(param)
--- 		for id, area in pairs(areas.areas) do
--- 			if id == target_id then
--- 				local pos = areas.areas[id].pos1
--- 				if areas.areas[id].pos2.y > pos.y then
--- 				  pos = areas.areas[id].pos2
--- 				end
--- 				minetest.get_player_by_name(name):setpos(pos)
--- 				minetest.log("info", name.." visited area: "..param)
--- 				return
--- 			end		  
--- 		end		
--- 	end
--- })
-
-
-
-
-
-
 -- Area teleportation
 
 local tpamode  = minetest.setting_get ("tpamode")
